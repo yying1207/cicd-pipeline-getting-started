d34ae37ff6e7b41de7e794584cdefe152c8b9e1e
yying1207 <1022949490@qq.com>
2026-04-15 20:57:54 +0800

    added new function and test case

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,6 +1,11 @@
-from src.main import add
+from src.main import add, subtract
 
 def test_add_function():
     assert add(2, 3) == 5
     assert add(0, 0) == 0
     assert add(5, 5) == 10
+
+def test_subtract_function():
+    assert subtract(5, 3) == 2
+    assert subtract(0, 0) == 0
+    assert subtract(10, 5) == 5
